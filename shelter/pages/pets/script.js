2263088890c140f2af92fc3180f0cc08c6085920
Aoi Haru <snakeinwater1@gmail.com>
2022-04-18 15:46:21 +0300

    feat: add burger close animation

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,12 +1,17 @@
 const burgerIcon = document.querySelector('.hamburger-lines');
 const popupMenu = document.querySelector('.pop-up-menu');
 const header = document.querySelector('.header');
-const body = document.querySelector('.body')
+const body = document.querySelector('.body');
+const popupItems = document.querySelectorAll('.popup__item');
 
 
-burgerIcon.addEventListener('click', () => {
+const burgerToggle = () => {
   burgerIcon.classList.toggle('burger-flip');
   header.classList.toggle('header-pets-change');
   popupMenu.classList.toggle('popup-right');
   body.classList.toggle('black-bg')
-})
+}
+
+burgerIcon.addEventListener('click', burgerToggle);
+
+popupItems.forEach(x => x.addEventListener('click', burgerToggle))
