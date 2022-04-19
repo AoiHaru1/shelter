4e7a7bd09f763dedd1217fa91628e59bb1ea1a92
Aoi Haru <snakeinwater1@gmail.com>
2022-04-19 06:37:43 +0300

    fix: fix bg

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,6 +1,7 @@
 const burgerIcon = document.querySelector('.hamburger-lines');
 const popupMenu = document.querySelector('.pop-up-menu');
 const header = document.querySelector('.header');
+const header1 = document.querySelector('.header-fixer');
 const body = document.querySelector('.body');
 const popupItems = document.querySelectorAll('.popup__item');
 
@@ -8,6 +9,7 @@ const popupItems = document.querySelectorAll('.popup__item');
 const burgerToggle = () => {
   burgerIcon.classList.toggle('burger-flip');
   header.classList.toggle('header-pets-change');
+  header1.classList.toggle('header-pets-change');
   popupMenu.classList.toggle('popup-right');
   body.classList.toggle('black-bg')
 }
